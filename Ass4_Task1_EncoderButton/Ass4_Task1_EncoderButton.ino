9e37e6069af0357a89f8ac6f581674a3fe6f7ff8
Ayden1807 <atmosler@ncsu.edu>
2024-02-10 15:18:21 -0500

    Create Ass4_Task1_EncoderButton.ino

---
+++ Ass4_Task1_EncoderButton/Ass4_Task1_EncoderButton.ino
@@ -0,0 +1,40 @@
+ #define output_encoderCLK 18
+ #define output_encoderDATA 19
+ #define output_encoderSW 5
+
+ int counter = 0; 
+ int aState;
+ int aLastState;  
+ int SW_state;
+
+ void setup() { 
+   pinMode (output_encoderCLK,INPUT);
+   pinMode (output_encoderDATA,INPUT);
+   pinMode (output_encoderSW, INPUT);
+   
+   Serial.begin (9600);
+   // Reads the initial state of the outputA
+   aLastState = digitalRead(output_encoderCLK);   
+ } 
+
+ void loop() { 
+   aState = digitalRead(output_encoderCLK); // Reads the "current" state of the outputA
+   // If the previous and the current state of the outputA are different, that means a Pulse has occured
+   if (aState != aLastState){     
+     // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
+     if (digitalRead(output_encoderDATA) != aState) { 
+       counter ++;
+     } else {
+       counter --;
+     }
+     Serial.print("Position: ");
+     Serial.println(counter);
+   } 
+   SW_state = digitalRead(output_encoderSW);
+
+   if (SW_state == LOW) {
+    counter = 0;
+    Serial.println("Counter Reset");
+   }
+   aLastState = aState; // Updates the previous state of the outputA with the current state
+ }
