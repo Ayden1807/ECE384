4c0699bbff30e2f109bce864e99ce4df24ccd277
Ayden1807 <atmosler@ncsu.edu>
2024-02-10 21:59:24 -0500

    Create Ass4_Task2_LightSensor.ino

---
+++ Ass4_Task2_LightSensor/Ass4_Task2_LightSensor.ino
@@ -0,0 +1,19 @@
+#define output_Photoresistor 4
+
+int PR_value;
+
+void setup() {
+  pinMode(output_Photoresistor, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Read Analog to Digital Ouput of PR
+  PR_value = analogRead(output_Photoresistor);
+
+  Serial.print("Light Value: ");
+  Serial.println(PR_value);
+  delay(5000);
+  // delay(5000) = 5sec
+}
