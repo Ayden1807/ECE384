0e79a8895e60f03bee4e65fe21a0726d12b9d016
Ayden1807 <atmosler@ncsu.edu>
2024-02-23 01:21:08 -0500

    Update Midterm.ino

--- Midterm/Midterm.ino
+++ Midterm/Midterm.ino
@@ -2,10 +2,27 @@
 #include <LiquidCrystal_PCF8574.h>
 #include <DHT11.h>
 
+#define encoderCLK 18
+#define encoderDATA 19
+#define encoderSW 13
 #define DHTPIN 17
-#define BUTTON_PIN 5      // Change to pin 5 for the button
-#define LED_BUILTIN 13
+#define BUTTON_PIN 13
 
+// interrupt
+volatile int desiredMode = -1;
+
+// modeSelect
+int encoderPos = 0;
+int counter = 0;
+int aState;
+int aLastState;  
+int SW_state;
+
+// BUTTON???
+int buttonState = 0;
+int lastButtonState = 0;
+
+// from the old code
 DHT11 dht(DHTPIN);
 LiquidCrystal_PCF8574 lcd(0x27);
 
@@ -23,7 +40,24 @@ void IRAM_ATTR isr() {
   button1.pressed = true;
 }
 
+// Define different modes
+enum Mode {
+  REGULAR,
+  DEBUG,
+  CONSTANT_DISPLAY,
+  POWER_SAVE,
+  ON_REQUEST_SAVE_DATA,
+  HIBERNATE_STANDBY
+};
+
+Mode currentMode = REGULAR;
+
 void setup() {
+  pinMode(encoderCLK, INPUT);
+  pinMode(encoderDATA, INPUT);
+  pinMode(encoderSW, INPUT);
+
+  // from the old code
   int error;
   float temperature = 0;
   float humidity = 0;
@@ -53,49 +87,81 @@ void setup() {
   lcd.print("PRESS BUTTON!");
 
   pinMode(button1.PIN, INPUT_PULLUP);
-  pinMode(LED_BUILTIN, OUTPUT);
   attachInterrupt(button1.PIN, isr, FALLING);
+
+  aLastState = digitalRead(encoderCLK);
 }
+
+void modeSelect();
+void REGULAR_MODE();
 
 void loop() {
-  if (button1.pressed) {
-    button1.pressed = false;
-    lcd.setBacklight(225);
-    displayTemperatureAndHumidity();
-  }
+    modeSelect();
 
-  if (millis() - displayStartTime > 5000) {
-    // Turn off the display and backlight after 5 seconds
-    lcd.clear();
-    lcd.noDisplay();
-    lcd.setBacklight(0);
-  }
+    // Switch modes based on encoder position
+    switch (encoderPos) {
+        case 0:
+            currentMode = REGULAR;
+            break;
+        case 1:
+            currentMode = DEBUG;
+            break;
+        case 2:
+            currentMode = CONSTANT_DISPLAY;
+            break;
+        case 3:
+            currentMode = POWER_SAVE;
+            break;
+        case 4:
+            currentMode = ON_REQUEST_SAVE_DATA;
+            break;
+        case 5:
+            currentMode = HIBERNATE_STANDBY;
+            break;
+        // Add more cases as needed
+
+        // Wrap-around for continuous mode switching
+        default:
+            encoderPos = 0;
+            break;
+    }
+
+    // Perform actions based on the current mode
+    switch (currentMode) {
+        case REGULAR:
+            lcd.clear();
+            lcd.print("Regular Mode");
+            REGULAR_MODE();
+            break;
+        case DEBUG:
+            lcd.clear();
+            lcd.print("Debug Mode");
+            // Debug mode actions
+            break;
+        case CONSTANT_DISPLAY:
+            lcd.clear();
+            lcd.print("Constant Display Mode");
+            // Constant display mode actions
+            break;
+        case POWER_SAVE:
+            lcd.clear();
+            lcd.print("Power Save Mode");
+            // Power save mode actions
+            break;
+        case ON_REQUEST_SAVE_DATA:
+            lcd.clear();
+            lcd.print("On Request Mode");
+            // On request save data mode actions
+            break;
+        case HIBERNATE_STANDBY:
+            lcd.clear();
+            lcd.print("Hibernate/Standby Mode");
+            // Hibernate/standby mode actions
+            break;
+        // Add more cases as needed
+    }
+
+    // Add delay if necessary to avoid rapid menu changes
+    delay(100);
 }
 
-void displayTemperatureAndHumidity() {
-  lcd.clear();
-  lcd.display();
-
-  int temperature = 0;
-  int humidity = 0;
-  int chk = dht.readTemperatureHumidity(temperature, humidity);
-
-  if (chk == 0) {
-    lcd.home();
-    lcd.print("Temp: ");
-    lcd.print(temperature);
-    lcd.write(223);
-    lcd.print("C");
-
-    lcd.setCursor(0, 1);
-    lcd.print("Humidity: ");
-    lcd.print(humidity);
-    lcd.print("%");
-  } else {
-    Serial.print("DHT11 Read Error: ");
-    Serial.println(chk);
-  }
-
-  // Save the current time for display timeout
-  displayStartTime = millis();
-}
